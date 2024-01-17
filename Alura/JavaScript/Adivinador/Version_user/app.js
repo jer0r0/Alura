b033e8dea78612dce9baae025ebcc52d3762f2d7
Jeroro <97175757+jer0r0@users.noreply.github.com>
2024-01-17 15:59:24 -0500

    Add files via upload

---
+++ Alura/JavaScript/Adivinador/Version_user/app.js
@@ -0,0 +1,32 @@
+let numeroSecreto = 6;
+let maximos_int = 5;
+
+
+//console.log(numeroUsuario);
+/*
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}else{
+     alert('No adivinaste el numero correcto');
+} 
+*/
+
+function num_correct() {
+    if(maximos_int > 0){
+        let numeroUsuario = prompt("Me indicas un numero por favor:");
+        if (numeroUsuario == numeroSecreto) {
+            alert('Acertaste el numero');
+            return true;
+        }else{
+            alert('No adivinaste el numero correcto, prueba de nuevo, intentos restantes: '+ maximos_int);
+            maximos_int-= 1;
+            num_correct();
+            return false;
+        }
+    }else{
+        alert('El numero maximo de intentos ha sido alcanzado, ¡Perdiste!');
+    }
+        
+  }
+
+num_correct();
